8987b90d3aa07f3cde892a7a3a3296b804946827
Alex Lyubakov <lyubakovaa@gmail.com>
2018-11-14 13:37:09 +0400

    1,2,3,4,5 done

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
+  let div = document.createElement('div');
+  div.textContent = text;
 }
-
 /*
  Задание 2:
 
@@ -22,6 +23,7 @@ function createDivWithText(text) {
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
+  where.insertBefore(what, where.firstChild);
 }
 
 /*
@@ -44,6 +46,13 @@ function prepend(what, where) {
    findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
  */
 function findAllPSiblings(where) {
+  elems = [];
+  for (const child of where.children) {
+      if (child.nextElementSibling.tagName === "P") {
+          elems.push(child);
+      }
+  }
+  return elems;
 }
 
 /*
@@ -66,7 +75,7 @@ function findAllPSiblings(where) {
 function findError(where) {
     var result = [];
 
-    for (var child of where.childNodes) {
+    for (var child of where.children) {
         result.push(child.innerText);
     }
 
@@ -86,6 +95,11 @@ function findError(where) {
    должно быть преобразовано в <div></div><p></p>
  */
 function deleteTextNodes(where) {
+  for (const child of where.childNodes) {
+    if (child.nodeType === 3) {
+      where.removeChild(child);
+    }
+  }
 }
 
 /*
@@ -101,6 +115,14 @@ function deleteTextNodes(where) {
    должно быть преобразовано в <span><div><b></b></div><p></p></span>
  */
 function deleteTextNodesRecursive(where) {
+  let nodes = where.childNodes;
+  for (const child of nodes.childNodes) {
+    if (child.nodeType === 3) {
+      where.removeChild(child);
+    } else {
+      deleteTextNodesRecursive(child);
+    }
+  }
 }
 
 /*
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function createDivWithText(text) {
+  let div = document.createElement('div');
+  div.textContent(text);
+  console.log(div);
+}
+createDivWithText('loft');
